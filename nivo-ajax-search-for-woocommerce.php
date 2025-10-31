6deb275b51a250094011870f40795275ecf73404
Sakib <nazmunsakib81@gmail.com>
2025-11-01 00:24:10 +0600

    Fix Git conflict markers in main plugin file

--- nivo-ajax-search-for-woocommerce.php
+++ nivo-ajax-search-for-woocommerce.php
@@ -18,16 +18,11 @@
  * WC tested up to: 8.5
  * Network: false
  *
-<<<<<<< HEAD
  * @package NivoSearch
-=======
- * @package NASFWC
->>>>>>> 643a01bd53cd81e4a6e0b6db43d1b2086a4ddc58
  * @author Nazmun Sakib
  * @since 1.0.0
  */
 
-<<<<<<< HEAD
 // Prevent direct access.
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
@@ -39,17 +34,6 @@ define( 'NIVO_SEARCH_PLUGIN_FILE', __FILE__ );
 define( 'NIVO_SEARCH_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'NIVO_SEARCH_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'NIVO_SEARCH_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
-=======
-// Prevent direct access
-defined('ABSPATH') || exit;
-
-// Define plugin constants
-define('NASFWC_VERSION', '1.0.0');
-define('NASFWC_PLUGIN_FILE', __FILE__);
-define('NASFWC_PLUGIN_DIR', plugin_dir_path(__FILE__));
-define('NASFWC_PLUGIN_URL', plugin_dir_url(__FILE__));
-define('NASFWC_PLUGIN_BASENAME', plugin_basename(__FILE__));
->>>>>>> 643a01bd53cd81e4a6e0b6db43d1b2086a4ddc58
 
 /**
  * Declare WooCommerce HPOS compatibility
@@ -57,7 +41,6 @@ define('NASFWC_PLUGIN_BASENAME', plugin_basename(__FILE__));
  * @since 1.0.0
  * @return void
  */
-<<<<<<< HEAD
 add_action(
 	'before_woocommerce_init',
 	function() {
@@ -66,13 +49,6 @@ add_action(
 		}
 	}
 );
-=======
-add_action('before_woocommerce_init', function() {
-    if (class_exists('\Automattic\WooCommerce\Utilities\FeaturesUtil')) {
-        \Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility('custom_order_tables', __FILE__, true);
-    }
-});
->>>>>>> 643a01bd53cd81e4a6e0b6db43d1b2086a4ddc58
 
 /**
  * Initialize the plugin
@@ -80,7 +56,6 @@ add_action('before_woocommerce_init', function() {
  * @since 1.0.0
  * @return void
  */
-<<<<<<< HEAD
 function nivo_search_init() {
 	// Load Composer autoloader.
 	if ( file_exists( NIVO_SEARCH_PLUGIN_DIR . 'vendor/autoload.php' ) ) {
@@ -93,20 +68,6 @@ function nivo_search_init() {
 
 // Hook initialization.
 add_action( 'plugins_loaded', 'nivo_search_init' );
-=======
-function nasfwc_init() {
-    // Load Composer autoloader
-    if (file_exists(NASFWC_PLUGIN_DIR . 'vendor/autoload.php')) {
-        require_once NASFWC_PLUGIN_DIR . 'vendor/autoload.php';
-    }
-    
-    // Initialize main plugin class
-    NASFWC\Nivo_Ajax_Search::get_instance();
-}
-
-// Hook initialization
-add_action('plugins_loaded', 'nasfwc_init');
->>>>>>> 643a01bd53cd81e4a6e0b6db43d1b2086a4ddc58
 
 /**
  * Add settings link to plugin action links
@@ -115,7 +76,6 @@ add_action('plugins_loaded', 'nasfwc_init');
  * @param array $links Plugin action links
  * @return array Modified plugin action links
  */
-<<<<<<< HEAD
 add_filter(
 	'plugin_action_links_' . NIVO_SEARCH_PLUGIN_BASENAME,
 	function( $links ) {
@@ -124,13 +84,6 @@ add_filter(
 		return $links;
 	}
 );
-=======
-add_filter('plugin_action_links_' . NASFWC_PLUGIN_BASENAME, function($links) {
-    $settings_link = '<a href="' . admin_url('admin.php?page=nasfwc-settings') . '">' . __('Settings', 'nivo-ajax-search-for-woocommerce') . '</a>';
-    array_unshift($links, $settings_link);
-    return $links;
-});
->>>>>>> 643a01bd53cd81e4a6e0b6db43d1b2086a4ddc58
 
 /**
  * Plugin activation hook
@@ -138,7 +91,6 @@ add_filter('plugin_action_links_' . NASFWC_PLUGIN_BASENAME, function($links) {
  * @since 1.0.0
  * @return void
  */
-<<<<<<< HEAD
 register_activation_hook(
 	__FILE__,
 	function() {
@@ -149,15 +101,6 @@ register_activation_hook(
 		}
 	}
 );
-=======
-register_activation_hook(__FILE__, function() {
-    // Check WooCommerce dependency
-    if (!class_exists('WooCommerce')) {
-        deactivate_plugins(plugin_basename(__FILE__));
-        wp_die(__('This plugin requires WooCommerce to be installed and active.', 'nivo-ajax-search-for-woocommerce'));
-    }
-});
->>>>>>> 643a01bd53cd81e4a6e0b6db43d1b2086a4ddc58
 
 /**
  * Plugin deactivation hook
@@ -165,18 +108,12 @@ register_activation_hook(__FILE__, function() {
  * @since 1.0.0
  * @return void
  */
-<<<<<<< HEAD
 register_deactivation_hook(
 	__FILE__,
 	function() {
 		// Cleanup if needed.
 	}
 );
-=======
-register_deactivation_hook(__FILE__, function() {
-    // Cleanup if needed
-});
->>>>>>> 643a01bd53cd81e4a6e0b6db43d1b2086a4ddc58
 
 /**
  * Add plugin meta links
@@ -186,7 +123,6 @@ register_deactivation_hook(__FILE__, function() {
  * @param string $file Plugin file
  * @return array Modified plugin meta links
  */
-<<<<<<< HEAD
 add_filter(
 	'plugin_row_meta',
 	function( $links, $file ) {
@@ -199,12 +135,3 @@ add_filter(
 	10,
 	2
 );
-=======
-add_filter('plugin_row_meta', function($links, $file) {
-    if ($file === NASFWC_PLUGIN_BASENAME) {
-        $links[] = '<a href="https://github.com/nazmunsakib/nivo-ajax-search-for-woocommerce" target="_blank">' . __('Documentation', 'nivo-ajax-search-for-woocommerce') . '</a>';
-        $links[] = '<a href="https://github.com/nazmunsakib/nivo-ajax-search-for-woocommerce/issues" target="_blank">' . __('Support', 'nivo-ajax-search-for-woocommerce') . '</a>';
-    }
-    return $links;
-}, 10, 2);
->>>>>>> 643a01bd53cd81e4a6e0b6db43d1b2086a4ddc58
